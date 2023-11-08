72aad56804d385192c2ca0faeb8518d688d64a82
Mikhail Okhapkin <mikhail.okhapkin@yandex.ru>
2023-11-08 20:42:43 +0300

    Initialization of the project

---
+++ src/main/java/ru/rtu_mirea/migrationdb/MigrationDbApplication.java
@@ -0,0 +1,13 @@
+package ru.rtu_mirea.migrationdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MigrationDbApplication.class, args);
+    }
+
+}
