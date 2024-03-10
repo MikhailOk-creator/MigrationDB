3015594bfc6ea72c87048baf29572bd45efce9a8
Mikhail Okhapkin <mikhail.okhapkin@yandex.ru>
2024-03-10 20:24:04 +0300

    Made a web interface Now the application has its own small web interface. This interface is needed to enter connection data and display information about the migration status.

---
+++ src/main/java/ru/rtu_mirea/migrationdb/controller/WebController.java
@@ -0,0 +1,37 @@
+package ru.rtu_mirea.migrationdb.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.rtu_mirea.migrationdb.entity.ConnectionsDataDTO;
+
+@Controller
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class WebController {
+    private final MigrationController migrate;
+    @GetMapping("")
+    public String index() {
+        return "index";
+    }
+
+    @PostMapping("/migrate_web")
+    public String migrateWeb(@ModelAttribute ConnectionsDataDTO connectionsDataDTO, Model model) {
+        try {
+            String sourceDB = connectionsDataDTO.getDatabase1();
+            String targetDB = connectionsDataDTO.getDatabase2();
+            ResponseEntity<?> response = migrate.migrate(connectionsDataDTO);
+            model.addAttribute("status", sourceDB + " -> " + targetDB + ": " + response.getBody());
+            return "index";
+        } catch (Exception e) {
+            model.addAttribute("status", "Migration failed: " + e.getMessage());
+            return "error";
+        }
+    }
+}
