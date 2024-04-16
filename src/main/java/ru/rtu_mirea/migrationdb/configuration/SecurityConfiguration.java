bff60475716a76c2b711877a72fa610f6de1dcf5
Mikhail Okhapkin <mikhail.okhapkin@yandex.ru>
2024-04-16 16:48:33 +0300

    Added security The project now supports Spring Security. The password is generated when the application is started.

---
+++ src/main/java/ru/rtu_mirea/migrationdb/configuration/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package ru.rtu_mirea.migrationdb.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration corsConfiguration = new CorsConfiguration();
+                    corsConfiguration.setAllowedOriginPatterns(List.of("*"));
+                    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    corsConfiguration.setAllowedHeaders(List.of("*"));
+                    corsConfiguration.setAllowCredentials(true);
+                    return corsConfiguration;
+                }))
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/").authenticated()
+                        .requestMatchers("/migrate").authenticated()
+                        .requestMatchers("/history").authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+}
